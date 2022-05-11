018108fbf7c9f9b5855c3019bd9f50644dba21f4
tylerjusfly <tylerjusfly1@gmail.com>
2022-05-11 23:20:42 +0100

    added 2nd challenge ♣♥ sameday

--- Define-A-card-suit.js
+++ Define-A-card-suit.js
@@ -29,5 +29,13 @@ function defineSuit(card) {
       break;
   }
 }
+// favorite solution
 
-console.log(defineSuit('3♥'))
+function defineSuit(card) {
+  if(card.includes('♥')) return 'hearts'
+  if(card.includes('♦')) return 'diamonds'
+  if(card.includes('♣')) return 'clubs'
+  if(card.includes('♠')) return 'spades' 
+  }
+
+console.log(defineSuit('3♠'))
