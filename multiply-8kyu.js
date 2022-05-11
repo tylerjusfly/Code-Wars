3fe3cc98a08f0244a0b33e782c8e6fcd67f595ed
tylerjusfly <tylerjusfly1@gmail.com>
2022-05-11 11:06:31 +0100

    added my first codewars 😎😎

---
+++ multiply-8kyu.js
@@ -0,0 +1,16 @@
+/* 
+This code does not execute properly. Try to figure out why.
+
+function multiply(a, b){
+  a * b
+}
+
+*/
+
+function multiply(a, b){
+  // Simply added a return
+   return a * b
+}
+
+console.log(multiply(2, 2))
+// result equals 4
