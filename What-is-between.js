edbcf9e687c13fca24cb6746342136256a7774ec
Momoh Tyler <53145644+tylerjusfly@users.noreply.github.com>
2022-05-16 20:22:25 +0100

    Update What-is-between.js

--- What-is-between.js
+++ What-is-between.js
@@ -1,37 +1,22 @@
 /*
 Complete the function that takes two integers (a, b, where a < b) and return an array of all integers between the input parameters, including them.
 */
-// Parameters - 2 Integers (a, b) where a is less than b
-// Return - must return all numbers between the input .
-/* Example 
-  (1, 4) => {
-    return [1, 2, 3, 4]
-  }
-  const between = (a, b) => {
-  let res = []
-  for(let i = a; i <= b; i++) {
-    res.push(i);   
-  }  
-  return res;
-}
-  */
-
-/* Psudocode
-  create an empty array 
-  loop through the array.
-  add 1 to the initial value and push to the empty array
-*/
 function between(a, b) {
-  // your code here
-  let result = [];
-  let end = b - a;
+//   P  We are receiving integers(1,4)
+//   R We are Returning the numbers and the numbers in btw
+//   E (1, 6) => [1, 2, 3, 4, 5, 6]
+//   P we get the differnece btw the number by subtracting
+//   the bigger number has to come first so it doesn't return a negative
+//   we loop through to the end and add 1 to each number looped
+//   and push each number to an array
   
-  for (let i = 0; i <= end; i++) {
-  let sum = a + i;
-  result.push(sum);
+  let arraysOfNum = []
+  let btw = b - a
+  for(let i = 0; i <= btw; i++){
+    let sum = a + i
+    arraysOfNum.push(sum)
   }
-  return result;
- 
+  return arraysOfNum
 }
 
 
