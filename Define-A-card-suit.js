6951bf90bf99fb50d31a1b5dde57f0ce9b8cf1fb
tylerjusfly <tylerjusfly1@gmail.com>
2022-06-03 10:22:45 +0100

    active recalling of katas

--- Define-A-card-suit.js
+++ Define-A-card-suit.js
@@ -39,3 +39,19 @@ function defineSuit(card) {
   }
 
 console.log(defineSuit('3♠'))
+
+function defineSuit(card) {
+  // good luck
+  if (card[card.length -1] == '♣'){
+    return 'clubs'
+  }
+  else if(card[card.length -1] == '♦'){
+    return 'diamonds'
+  }
+  else if(card[card.length -1] == '♥'){
+    return 'hearts'
+  }
+  else if(card[card.length -1 ] == '♠'){
+     return 'spades'
+  }
+}
