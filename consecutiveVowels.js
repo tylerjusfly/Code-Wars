632b93d1e1f97e9b0933c126ebfa163088b1291c
tylerjusfly <tylerjusfly1@gmail.com>
2022-06-19 14:55:15 +0100

    adding a more cleaner solution

--- consecutiveVowels.js
+++ consecutiveVowels.js
@@ -18,59 +18,59 @@ Good luck!
 Strings
 */
 
-// function getTheVowels(string) {
-//     // P => ramdom strings (lowercase)
-//     // R => returning a length of consecutive or vowels that folllows each other
-//     // E => (raidme) => (2)
-//     // P => create an array of vowels, split the parameters received into an array. also set a count to keep track of how many vowels found
-//     //  we then iterate through the array, check if the iterated value is equals to the first index in the array of vowels. 
-//     // if true , increase count, and remove the index off and run func again , till all iterated value has been checked
-
-//     // specifying array of vowels
-//     let arrayOfVowels = ['a','e','i','o','u'];
-//     let count = 0;
-//     // turning string received to an array
-
-//     let arrayOfLetters = string.split('');
-
-//     //Iterrate Through
-//     for(let i = 0; i < arrayOfLetters.length; i++){
-//         //checking if found the first index in the array of letters looped
-//         if(arrayOfVowels[0] == arrayOfLetters[i]){
-//             count ++    //increase Count
-//             arrayOfVowels.shift() // remove the first index
-//         }
-//         if(arrayOfVowels.length == 0){
-//             // Reset the array of vowels
-//             arrayOfVowels = ['a','e','i','o','u'];
-//         }
-//     }
-    
-//     return count
-// }
-
-
 function getTheVowels(string) {
+    // P => ramdom strings (lowercase)
+    // R => returning a length of consecutive or vowels that folllows each other
+    // E => (raidme) => (2)
+    // P => create an array of vowels, split the parameters received into an array. also set a count to keep track of how many vowels found
+    //  we then iterate through the array, check if the iterated value is equals to the first index in the array of vowels. 
+    // if true , increase count, and remove the index off and run func again , till all iterated value has been checked
 
     // specifying array of vowels
     let arrayOfVowels = ['a','e','i','o','u'];
     let count = 0;
     // turning string received to an array
+
     let arrayOfLetters = string.split('');
-  
-    // A more Cleaner way
-    for(let index in arrayOfLetters){
-        if(arrayOfVowels[0] == arrayOfLetters[index]){
-            count ++
-            arrayOfVowels.shift()
+
+    //Iterrate Through
+    for(let i = 0; i < arrayOfLetters.length; i++){
+        //checking if found the first index in the array of letters looped
+        if(arrayOfVowels[0] == arrayOfLetters[i]){
+            count ++    //increase Count
+            arrayOfVowels.shift() // remove the first index
         }
         if(arrayOfVowels.length == 0){
-            arrayOfVowels =  ['a','e','i','o','u'];
-       }
-
+            // Reset the array of vowels
+            arrayOfVowels = ['a','e','i','o','u'];
+        }
     }
+    
     return count
 }
+
+
+// function getTheVowels(string) {
+
+//     // specifying array of vowels
+//     let arrayOfVowels = ['a','e','i','o','u'];
+//     let count = 0;
+//     // turning string received to an array
+//     let arrayOfLetters = string.split('');
+  
+//     // A more Cleaner way
+//     for(let index in arrayOfLetters){
+//         if(arrayOfVowels[0] == arrayOfLetters[index]){
+//             count ++
+//             arrayOfVowels.shift()
+//         }
+//         if(arrayOfVowels.length == 0){
+//             arrayOfVowels =  ['a','e','i','o','u'];
+//        }
+
+//     }
+//     return count
+// }
 
    // const s = 'akfheujfkgiaaaofmmfkdfuaiiie';
     const s = 'raidme'
