8cff414211497c68e42a3e788b99f61037a39fca
tylerjusfly <tylerjusfly1@gmail.com>
2022-07-05 10:50:46 +0100

    adding sum of intervals

--- 7kyu/sum-of-intervals.js
+++ 7kyu/sum-of-intervals.js
@@ -1,1 +1,30 @@
 //https://www.codewars.com/kata/52b7ed099cdc285c300001cd
+
+// THIS FAILED ONE TEST
+
+function removeSmallest(numbers) {
+    //   Return an empty list if empty
+    //   given an array , return the smallest value
+      //throw "TODO: removeSmallest";
+    //   first check if array is empty , return back empty array
+    // else if array is > 0 
+      if(numbers.length == 0){
+        return []
+      }
+      else{
+        // get a copy
+        let copy = [...numbers]
+        let low = Math.min(...numbers)
+        
+        let index = numbers.indexOf(low)
+        copy.splice(index, 1)
+        return copy
+      }
+    }
+
+
+
+let numbers = [ 225, 150, 124, 399, 300, 124]
+// [ 225, 150, 399, 300 ] 
+
+console.log(removeSmallest(numbers))
