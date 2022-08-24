1c758d1a48cc205a709d01c5e8e8aefb0ae65f2d
tylerjusfly <tylerjusfly2@gmail.com>
2022-08-24 14:59:59 +0100

    friends or foe

---
+++ php-codewars/friendsOrFoe.php
@@ -0,0 +1,29 @@
+<!-- Make a program that filters a list of strings and returns a list with only your friends name in it.
+
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+
+Ex: Input = ["Ryan", "Kieran", "Jason", "Yous"], Output = ["Ryan", "Yous"]
+
+i.e.
+
+friend ["Ryan", "Kieran", "Mark"] `shouldBe` ["Ryan", "Mark"]
+
+Note: keep the original order of the names in the output. -->
+
+<?php
+
+    function friendsOrFoe($input){
+        $new_array = array();
+
+        foreach($input as $value){
+            if(strlen($value) == 4){
+                array_push($new_array, $value);
+            }
+        }
+        return $new_array;
+    }
+
+    $input = ["Ryan", "Kieran", "Jason", "Yous"];
+
+    print_r(friendsOrFoe($input))
+?>
